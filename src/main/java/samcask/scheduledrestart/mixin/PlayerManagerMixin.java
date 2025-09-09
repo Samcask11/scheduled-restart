a5404d235e19e92db8bf0aa67f0588d8fc78e105
Samcask <sam.caskey11@gmail.com>
2025-09-10 05:04:12 +1000

    Figured out mixins

--- src/main/java/samcask/scheduledrestart/mixin/PlayerManagerMixin.java
+++ src/main/java/samcask/scheduledrestart/mixin/PlayerManagerMixin.java
@@ -1,7 +1,10 @@
 package samcask.scheduledrestart.mixin;
 
+import net.minecraft.network.ClientConnection;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,15 +14,17 @@ import samcask.scheduledrestart.RestartNoPlayerScheduler;
 
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
-	@Shadow public abstract MinecraftServer getServer();
+	@Final
+	@Shadow
+    private MinecraftServer server;
 
-	@Inject(method = "onPlayerConnect", at = @At("HEAD"))
-	private void playerConnected(CallbackInfo info) {
+    @Inject(method = "onPlayerConnect", at = @At("HEAD"))
+	public void playerConnected(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
 		RestartNoPlayerScheduler.playerConnected();
 	}
 
 	@Inject(method = "remove", at = @At("TAIL"))
-	private void playerDisconnected(CallbackInfo info) {
-		RestartNoPlayerScheduler.playerDisconnected(getServer());
+	private void playerDisconnected(ServerPlayerEntity player, CallbackInfo info) {
+		RestartNoPlayerScheduler.playerDisconnected(server);
 	}
 }
