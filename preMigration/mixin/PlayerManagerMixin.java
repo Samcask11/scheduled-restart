7db375fe226af87008362e5eeb501b22137250f1
Samcask <sam.caskey11@gmail.com>
2026-03-26 19:40:12 +1100

    Migrate to Mojang mappings

--- preMigration/mixin/PlayerManagerMixin.java
+++ preMigration/mixin/PlayerManagerMixin.java
@@ -1,10 +1,10 @@
 package samcask.scheduledrestart.mixin;
 
-import net.minecraft.network.Connection;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.CommonListenerCookie;
-import net.minecraft.server.players.PlayerList;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,19 +13,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import samcask.scheduledrestart.scheduling.NoPlayerRestart;
 
-@Mixin(PlayerList.class)
+@Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
 	@Final
 	@Shadow
     private MinecraftServer server;
 
-    @Inject(method = "placeNewPlayer", at = @At("HEAD"))
-	public void playerConnected(Connection connection, ServerPlayer player, CommonListenerCookie clientData, CallbackInfo info) {
+    @Inject(method = "onPlayerConnect", at = @At("HEAD"))
+	public void playerConnected(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo info) {
 		NoPlayerRestart.playerConnected();
 	}
 
 	@Inject(method = "remove", at = @At("TAIL"))
-	private void playerDisconnected(ServerPlayer player, CallbackInfo info) {
+	private void playerDisconnected(ServerPlayerEntity player, CallbackInfo info) {
 		NoPlayerRestart.playerDisconnected(server);
 	}
 }
